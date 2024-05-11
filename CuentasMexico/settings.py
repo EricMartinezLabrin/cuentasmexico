6d21d334c7860540c30fa0a48887241d3d9afe7c
EricMartinezLabrin <98716898+EricMartinezLabrin@users.noreply.github.com>
2024-05-11 14:27:38 -0600

    chore: Update database configuration to use Roundhouse server

--- CuentasMexico/settings.py
+++ CuentasMexico/settings.py
@@ -33,9 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uf41q9_1%4#x4!k4a)pa#pqc&5aj^-s)*f5lcicaui-$m2@s*e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'cuentasmexico.mx']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -94,14 +94,15 @@ WSGI_APPLICATION = 'CuentasMexico.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# mysql://root:NbsAjgXbGdaGmXuAritURIPcORuXpdQY@roundhouse.proxy.rlwy.net:55263/railway
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'Tarkan11.-',
-        'HOST': 'NbsAjgXbGdaGmXuAritURIPcORuXpdQY@roundhouse.proxy.rlwy.net',
+        'PASSWORD': 'NbsAjgXbGdaGmXuAritURIPcORuXpdQY',
+        'HOST': 'roundhouse.proxy.rlwy.net',
         'PORT': '55263',
     }
 }
