a0b3e0237a4a962d0471d38ed8e215a37d88309e
EricMartinezLabrin <98716898+EricMartinezLabrin@users.noreply.github.com>
2024-05-12 12:48:55 -0600

    Update CSRF_TRUSTED_ORIGINS in settings.py

--- CuentasMexico/settings.py
+++ CuentasMexico/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-uf41q9_1%4#x4!k4a)pa#pqc&5aj^-s)*f5lcicaui-$m2@s*e
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','https://cuentasmexico-production.up.railway.app','cuentasmexico-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://cuentasmexico-production.up.railway.app','cuentasmexico-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://cuentasmexico-production.up.railway.app']
 
 
 # Application definition
