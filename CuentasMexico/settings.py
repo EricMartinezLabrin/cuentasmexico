2f63443afe9474710e857d6fccb7f77b7e731330
EricMartinezLabrin <98716898+EricMartinezLabrin@users.noreply.github.com>
2024-05-11 18:51:15 -0600

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS in settings.py

--- CuentasMexico/settings.py
+++ CuentasMexico/settings.py
@@ -35,7 +35,8 @@ SECRET_KEY = 'django-insecure-uf41q9_1%4#x4!k4a)pa#pqc&5aj^-s)*f5lcicaui-$m2@s*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','https://cuentasmexico-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','https://cuentasmexico-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://cuentasmexico-production.up.railway.app']
 
 
 # Application definition
