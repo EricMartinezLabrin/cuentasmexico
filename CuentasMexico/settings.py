0822b06a7fbf804df710988c8eabcf7004713266
Eric Martínez <ericmartinezlabrin@gmail.com>
2025-11-13 14:22:55 -0600

    Agregar cm.fadetechs.com a ALLOWED_HOSTS y CSRF_TRUSTED_ORIGINS en settings.py

--- CuentasMexico/settings.py
+++ CuentasMexico/settings.py
@@ -40,13 +40,13 @@ SECRET_KEY = 'django-insecure-uf41q9_1%4#x4!k4a)pa#pqc&5aj^-s)*f5lcicaui-$m2@s*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','www.cuentasmexico.mx','cuentasmexico.mx']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','www.cuentasmexico.mx','cuentasmexico.mx','cm.fadetechs.com']
 CSRF_TRUSTED_ORIGINS = [
     'https://www.cuentasmexico.mx',
     'https://cuentasmexico.mx',
     'http://localhost',
     'http://127.0.0.1',
-    
+    'https://cm.fadetechs.com'  
 ]
 
 
