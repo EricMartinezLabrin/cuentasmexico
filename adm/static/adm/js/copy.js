8daa59218c0c04676542db4d8417b9a88e294d9a
Eric Martínez <ericmartinezlabrin@gmail.com>
2026-01-18 13:40:53 -0600

    feat: Agregar vistas y plantillas para detalles de ventas web diarias, semanales, mensuales y anuales

--- adm/static/adm/js/copy.js
+++ adm/static/adm/js/copy.js
@@ -2,4 +2,9 @@ function copyAcc(copyId) {
   const codigoACopiar = document.getElementById(copyId);
   codigoACopiar.select();
   document.execCommand("copy");
+
+  // Mostrar notificación toast si la función existe
+  if (typeof showToast === 'function') {
+    showToast('Claves copiadas al portapapeles');
+  }
 }
